a4535534b79f3c4412d443cc070d5bdd98965f47
Luis Sanchez <lsanchez@vivial.net>
2020-01-08 21:55:36 -0400

    adds form

--- src/item_entry/item-entry.js
+++ src/item_entry/item-entry.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack, Input, Select, Textarea } from "@chakra-ui/core";
+import { Stack, Input, Select, Textarea, Button } from "@chakra-ui/core";
 
 // item [table]
 // id
@@ -10,45 +10,54 @@ import { Stack, Input, Select, Textarea } from "@chakra-ui/core";
 // tag
 // brand_id
 // type_id
+const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log('entered here', event.target)
 
+}
 const ItemEntry = () => {
+
+
   return (
-    <Stack width="30%" m={3}>
-      <Input placeholder="Item Name" />
-      <Select placeholder="Select item type">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Select placeholder="Select brand">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Select placeholder="Select model">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Select placeholder="Select status">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Select placeholder="Select user">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Select placeholder="Select department">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <Input placeholder="Serial No." />
-      <Input placeholder="Tag" />
-      <Textarea placeholder="Comments" />
-    </Stack>
+    <form onSubmit={handleSubmit}>
+      <Stack width="30%" m={3}>
+        <Input placeholder="Item Name" />
+        <Select placeholder="Select item type">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Select brand">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Select model">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Select status">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Select user">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Select department">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Input placeholder="Serial No." />
+        <Input placeholder="Tag" />
+        <Textarea placeholder="Comments" />
+        <Button type="submit" variantColor="green" size="md">Submit</Button>
+      </Stack>
+    </form>
   )
 }
 
