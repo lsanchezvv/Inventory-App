e2fb44bb9d565897c544630b7b742bea76593861
Luis Sanchez <lsanchez@vivial.net>
2020-01-09 21:14:13 -0400

    using react hooks to handle state

--- src/item_entry/item-entry.js
+++ src/item_entry/item-entry.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Stack, Input, Select, Textarea, Button } from "@chakra-ui/core";
 
 // item [table]
@@ -10,51 +10,107 @@ import { Stack, Input, Select, Textarea, Button } from "@chakra-ui/core";
 // tag
 // brand_id
 // type_id
-const handleSubmit = (event) => {
-  event.preventDefault()
-  console.log('entered here', event.target)
 
-}
+
 const ItemEntry = () => {
+  const [itemType, setItemType] = useState('')
+  const [brand, setBrand] = useState('')
+  const [model, setModel] = useState('')
+  const [status, setStatus] = useState('')
+  const [user, setUser] = useState('')
+  const [department, setDepartment] = useState('')
+  const [itemName, setItemName] = useState('')
+  const [serialNo, setSerialNo] = useState('')
+  const [tag, setTag] = useState('')
+  const [comments, setComments] = useState('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(
+      itemType
+      , brand
+      , model
+      , status
+      , user
+      , department
+      , itemName
+      , serialNo
+      , tag
+      , comments)
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <Stack width="30%" m={3}>
-        <Input placeholder="Item Name" />
-        <Select placeholder="Select item type">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Input
+          value={itemName}
+          onChange={e => setItemName(e.target.value)}
+          placeholder="Item Name"
+        />
+        <Select
+          id="itemType"
+          placeholder="Select your item type"
+          value={itemType}
+          onChange={e => setItemType(e.target.value)}
+        >
+          <option value="laptop">Laptop</option>
+          <option value="monitor">Monitor</option>
+          <option value="desktop">Desktop</option>
         </Select>
-        <Select placeholder="Select brand">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Select
+          id="brand"
+          placeholder="Select the brand"
+          value={brand}
+          onChange={e => setBrand(e.target.value)}
+        >
+          <option value="lenovo">Lenovo</option>
+          <option value="dell">Dell</option>
+          <option value="hp">HP</option>
         </Select>
-        <Select placeholder="Select model">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Select
+          id="model"
+          placeholder="Select the item model"
+          value={model}
+          onChange={e => setModel(e.target.value)}
+        >
+          <option value="thinkpad">Thinkpad</option>
+          <option value="t580">T580</option>
+          <option value="t460">T460</option>
         </Select>
-        <Select placeholder="Select status">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Select
+          id="status"
+          placeholder="Select the item status"
+          value={status}
+          onChange={e => setStatus(e.target.value)}
+        >
+          <option value="in-use">In Use</option>
+          <option value="available">Available</option>
+          <option value="defective">Defective</option>
         </Select>
-        <Select placeholder="Select user">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Select
+          id="user"
+          placeholder="Select the user"
+          value={user}
+          onChange={e => setUser(e.target.value)}
+        >
+          <option value="mike-miller">Mike Miller</option>
+          <option value="john-smith">John Smith</option>
+          <option value="marky-mark">Marky Mark</option>
         </Select>
-        <Select placeholder="Select department">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
+        <Select
+          id="department"
+          placeholder="Select the department"
+          value={department}
+          onChange={e => setDepartment(e.target.value)}
+        >
+          <option value="hr">HHRR</option>
+          <option value="finance">Finance</option>
+          <option value="prod">Production</option>
+          <option value="it">IT</option>
         </Select>
-        <Input placeholder="Serial No." />
-        <Input placeholder="Tag" />
-        <Textarea placeholder="Comments" />
+        <Input value={serialNo} onChange={e => setSerialNo(e.target.value)} placeholder="Serial No." />
+        <Input value={tag} onChange={e => setTag(e.target.value)} placeholder="Tag" />
+        <Textarea value={comments} onChange={e => setComments(e.target.value)} placeholder="Comments" />
         <Button type="submit" variantColor="green" size="md">Submit</Button>
       </Stack>
     </form>
