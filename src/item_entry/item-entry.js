5fb43fad58b503cdea44d6a390b75f4f3e9e9f26
Luis Sanchez <lsanchez@vivial.net>
2020-01-12 21:52:53 -0400

    Implements tags field

--- src/item_entry/item-entry.js
+++ src/item_entry/item-entry.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import {
-  Stack, Input, Select, Textarea, Button, FormControl
+  Stack, Input, Select, Textarea, Button, FormControl, Tag,
+  TagLabel,
+  TagCloseButton
 } from "@chakra-ui/core";
 
 // item [table]
@@ -23,7 +25,7 @@ const ItemEntry = () => {
   const [department, setDepartment] = useState('')
   const [itemName, setItemName] = useState('')
   const [serialNo, setSerialNo] = useState('')
-  const [tag, setTag] = useState('')
+  const [tags, setTags] = useState([])
   const [comments, setComments] = useState('')
 
   const clearForm = () => {
@@ -35,7 +37,7 @@ const ItemEntry = () => {
     setDepartment('')
     setItemName('')
     setSerialNo('')
-    setTag('')
+    setTags([])
     setComments('')
   }
 
@@ -50,100 +52,130 @@ const ItemEntry = () => {
       , department
       , itemName
       , serialNo
-      , tag
+      , tags
       , comments)
     clearForm()
   }
 
+  const handleTags = (e) => {
+    e.preventDefault()
+    if (e.key === 'Enter' && e.target.value !== '') {
+      setTags([...tags, e.target.value])
+      e.target.value = ''
+    }
+  }
+
+  const disableSubmitOnEnter = (e) => {
+    if (e.which === 13 /* Enter */) {
+      e.preventDefault();
+    }
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <Stack width="30%" m={3}>
-        <Input
-          value={itemName}
-          onChange={e => setItemName(e.target.value)}
-          placeholder="Item Name"
-        />
-        <FormControl isRequired>
-          <Select
-            id="itemType"
-            placeholder="Select your item type"
-            value={itemType}
-            onChange={e => setItemType(e.target.value)}
-          >
-            <option value="laptop">Laptop</option>
-            <option value="monitor">Monitor</option>
-            <option value="desktop">Desktop</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <Select
-            id="brand"
-            placeholder="Select the brand"
-            value={brand}
-            onChange={e => setBrand(e.target.value)}
-          >
-            <option value="lenovo">Lenovo</option>
-            <option value="dell">Dell</option>
-            <option value="hp">HP</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <Select
-            id="model"
-            placeholder="Select the item model"
-            value={model}
-            onChange={e => setModel(e.target.value)}
-          >
-            <option value="thinkpad">Thinkpad</option>
-            <option value="t580">T580</option>
-            <option value="t460">T460</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <Select
-            id="status"
-            placeholder="Select the item status"
-            value={status}
-            onChange={e => setStatus(e.target.value)}
-          >
-            <option value="in-use">In Use</option>
-            <option value="available">Available</option>
-            <option value="defective">Defective</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <Select
-            id="user"
-            placeholder="Select the user"
-            value={user}
-            onChange={e => setUser(e.target.value)}
-          >
-            <option value="mike-miller">Mike Miller</option>
-            <option value="john-smith">John Smith</option>
-            <option value="marky-mark">Marky Mark</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <Select
-            id="department"
-            placeholder="Select the department"
-            value={department}
-            onChange={e => setDepartment(e.target.value)}
-          >
-            <option value="hr">HHRR</option>
-            <option value="finance">Finance</option>
-            <option value="prod">Production</option>
-            <option value="it">IT</option>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <Input value={serialNo} onChange={e => setSerialNo(e.target.value)} placeholder="Serial No." />
-        </FormControl>
-        <Input value={tag} onChange={e => setTag(e.target.value)} placeholder="Tag" />
-        <Textarea value={comments} onChange={e => setComments(e.target.value)} placeholder="Notes" />
-        <Button type="submit" variantColor="green" size="md">Submit</Button>
-      </Stack>
-    </form>
+    <Fragment>
+      <form onSubmit={handleSubmit} onKeyPress={disableSubmitOnEnter}>
+        <Stack width="30%" m={3}>
+          <Input
+            value={itemName}
+            onChange={e => setItemName(e.target.value)}
+            placeholder="Item Name"
+          />
+          <FormControl isRequired>
+            <Select
+              id="itemType"
+              placeholder="Select your item type"
+              value={itemType}
+              onChange={e => setItemType(e.target.value)}
+            >
+              <option value="laptop">Laptop</option>
+              <option value="monitor">Monitor</option>
+              <option value="desktop">Desktop</option>
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <Select
+              id="brand"
+              placeholder="Select the brand"
+              value={brand}
+              onChange={e => setBrand(e.target.value)}
+            >
+              <option value="lenovo">Lenovo</option>
+              <option value="dell">Dell</option>
+              <option value="hp">HP</option>
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <Select
+              id="model"
+              placeholder="Select the item model"
+              value={model}
+              onChange={e => setModel(e.target.value)}
+            >
+              <option value="thinkpad">Thinkpad</option>
+              <option value="t580">T580</option>
+              <option value="t460">T460</option>
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <Select
+              id="status"
+              placeholder="Select the item status"
+              value={status}
+              onChange={e => setStatus(e.target.value)}
+            >
+              <option value="in-use">In Use</option>
+              <option value="available">Available</option>
+              <option value="defective">Defective</option>
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <Select
+              id="user"
+              placeholder="Select the user"
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            >
+              <option value="mike-miller">Mike Miller</option>
+              <option value="john-smith">John Smith</option>
+              <option value="marky-mark">Marky Mark</option>
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <Select
+              id="department"
+              placeholder="Select the department"
+              value={department}
+              onChange={e => setDepartment(e.target.value)}
+            >
+              <option value="hr">HHRR</option>
+              <option value="finance">Finance</option>
+              <option value="prod">Production</option>
+              <option value="it">IT</option>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Input value={serialNo} onChange={e => setSerialNo(e.target.value)} placeholder="Serial No." />
+          </FormControl>
+          <Textarea value={comments} onChange={e => setComments(e.target.value)} placeholder="Notes" />
+          <Input onKeyUp={e => handleTags(e)} placeholder="Tag" />
+          <Stack spacing={4} isInline>
+            {tags.map((tag, i) => (
+              <Tag
+                size="xs"
+                key={i}
+                rounded="full"
+                variant="solid"
+                variantColor="cyan"
+              >
+                <TagLabel>{tag}</TagLabel>
+                <TagCloseButton />
+              </Tag>
+            ))}
+          </Stack>
+          <Button type="submit" variantColor="green" size="md">Submit</Button>
+        </Stack>
+      </form>
+    </Fragment>
   )
 }
 
